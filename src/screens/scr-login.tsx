c9065c21f8bc084b728a94595f78c8f7fe0b2a71
Erik Leos <erikalejandrodl23@gmail.com>
2025-09-18 20:54:31 -0600

    Sketch

--- src/screens/scr-login.tsx
+++ src/screens/scr-login.tsx
@@ -4,17 +4,17 @@ import styles from "./scr-login.module.css";
 export function ScrLogIn() {
   return(
     <div className={styles.loginContainer}>
-      {/* <Image
+      <Image
         src="/Cat.png"
         alt="Login Cat"
         className={styles.loginCat}
         width={100}
         height={100}
-      /> */}
+      />
       <h1 className={styles.welcomeTitle}>Bienvenido</h1>
       <h2 className={styles.welcomeSubtitle}>Inicia sesion para empezar con tus deberes</h2>
       <form action="#" className={styles.loginForm}>
-        <p>Usuario</p>
+        <p>Correo Electronico</p>
         <div className={styles.inputContainer}>
           <Image src="/file.svg" width={24} height={24} alt="File Icon" />
           <input type="text" placeholder='example@mail.com' />
@@ -25,7 +25,7 @@ export function ScrLogIn() {
           <input type="password" placeholder='...'/>
         </div>
         <a href="#" className={styles.forgotPassword}>Contraseña olvidada</a>
-        <button type="submit">Iniciar Sesion</button>
+        <button type="submit" className={styles.loginButton}>Iniciar Sesion</button>
 
         <p className={styles.noAccount}>Todavia no tienes una cuenta</p>
         <a href="#" className={styles.createAccount}>Click aqui</a>
